ffe82e7af6f305468c30a0b7c2145f438e19b6ce
csyed <smachisty92@gmail.com>
2022-03-02 01:13:25 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -4,4 +4,9 @@ locals {
   ALL_EBS_IDS = aws_spot_instance_request.sopt_instance.*.ebs_block_device
   ALL_TAGS_IDS =   concat(local.ALL_INSTANCE_IDS, local.ALL_SPOT_IDS)
   TAG_NAME = "${var.COMPONENT}-${var.ENV}"
+  SPOT_VOLUME_ID = [for m in flatten(aws_spot_instance_request.sopt_instance.*.root_block_device) : m.volume_id]
+}
+
+output "EBS" {
+  value = aws_instance.od-instance
 }
