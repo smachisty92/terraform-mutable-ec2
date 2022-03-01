193abf2fd636f383691be3bdcfeba22691b9e759
csyed <smachisty92@gmail.com>
2022-03-01 20:53:16 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
   ALL_INSTANCE_IDS = concat(aws_instance.od-instance.*.id, aws_spot_instance_request.sopt_instance.*.id)
   ALL_SPOT_IDS = aws_spot_instance_request.sopt_instance.*.id
+  ALL_EBS_IDS = aws_spot_instance_request.sopt_instance.*.ebs_block_device
   ALL_TAGS_IDS =   concat(local.ALL_INSTANCE_IDS, local.ALL_SPOT_IDS)
   TAG_NAME = "${var.COMPONENT}-${var.ENV}"
 }
