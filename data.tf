a89ac1862f04988fe1a776f58e18285896dcfc25
csyed <smachisty92@gmail.com>
2022-03-01 19:39:40 +0530

    terraform

--- data.tf
+++ data.tf
@@ -1,5 +1,4 @@
 data "aws_ami" "ami" {
-  executable_users = ["self"]
   most_recent      = true
   name_regex       = "base-with-ansible" #AMI which was saved and which has ansible installed
   owners           = ["self"]
