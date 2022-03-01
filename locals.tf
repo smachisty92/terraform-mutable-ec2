aeaac10d6de815f424ff5de9c550bd4e602e6c7b
csyed <smachisty92@gmail.com>
2022-03-02 01:29:43 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
-  ALL_INSTANCE_IDS = concat(aws_instance.od-instance.*.id, aws_spot_instance_request.sopt_instance.*.spot_instance_id)
-  ALL_SPOT_IDS     = aws_spot_instance_request.sopt_instance.*.id
+  ALL_OD_INSTANCE_IDS = concat(aws_instance.od-instance.*.id, aws_spot_instance_request.sopt_instance.*.spot_instance_id)
+  ALL_SPOT_INSTANCE_IDS     = aws_spot_instance_request.sopt_instance.*.id
   ALL_EBS_IDS      = aws_spot_instance_request.sopt_instance.*.ebs_block_device
-  ALL_TAGS_IDS     = concat(local.ALL_INSTANCE_IDS, local.ALL_SPOT_IDS)
+  ALL_TAGS_IDS     = concat(local.ALL_OD_INSTANCE_IDS, local.ALL_SPOT_INSTANCE_IDS, local.ALL_VOLUMN_ID)
   TAG_NAME         = "${var.COMPONENT}-${var.ENV}"
   SPOT_VOLUME_ID   = [for m in flatten(aws_spot_instance_request.sopt_instance.*.root_block_device) : m.volume_id]
   OD_VOLUME_ID     = [for m in flatten(aws_instance.od-instance.*.root_block_device) : m.volume_id]
