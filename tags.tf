d08bf9d982bec65b847d11e3be260109b9eacfd6
csyed <smachisty92@gmail.com>
2022-03-03 02:59:14 +0530

    terraform

--- tags.tf
+++ tags.tf
@@ -1,29 +1,29 @@
-#
-#
-#module "tags" {
-#  count = length(local.ALL_TAGS)
-#  source = "git::https://github.com/smachisty92/terraform-tags"
-#  TAG_NAME = lookup(element(local.ALL_TAGS,count.index), "name")
-#  TAG_VALUE= lookup(element(local.ALL_TAGS,count.index), "value")
-#  RESOURCE_ID = local.ALL_TAGS_IDS
-#}
-#locals {
-#  ALL_TAGS= [
-#    {
-#      name = "Name"
-#      value = "${var.COMPONENT}-${var.ENV}"
-#    },
-#    {
-#      name = "env"
-#      value = var.ENV
-#    },
-#    {
-#      name = "component"
-#      value = var.COMPONENT
-#    },
-#    {
-#      name = "project_name"
-#      value = "roboshop"
-#    }
-#  ]
-#}
+
+
+module "tags" {
+  count = length(local.ALL_TAGS)
+  source = "git::https://github.com/smachisty92/terraform-tags"
+  TAG_NAME = lookup(element(local.ALL_TAGS,count.index), "name")
+  TAG_VALUE= lookup(element(local.ALL_TAGS,count.index), "value")
+  RESOURCE_ID = local.ALL_TAGS_IDS
+}
+locals {
+  ALL_TAGS= [
+    {
+      name = "Name"
+      value = "${var.COMPONENT}-${var.ENV}"
+    },
+    {
+      name = "env"
+      value = var.ENV
+    },
+    {
+      name = "component"
+      value = var.COMPONENT
+    },
+    {
+      name = "project_name"
+      value = "roboshop"
+    }
+  ]
+}
