929414a7f2ae00be53a17e49c5ff4893a8d3cae3
csyed <smachisty92@gmail.com>
2022-03-04 02:05:57 +0530

    terraform

--- alb.tf
+++ alb.tf
@@ -4,3 +4,10 @@ resource "aws_lb_target_group" "frontend" {
   protocol = "HTTP"
   vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
 }
+
+resource "aws_alb_target_group_attachment" "tg-attach" {
+  count = length(local.ALL_INSTANCE_IDS)
+  target_group_arn = aws_lb_target_group.frontend.arn
+  target_id        = element(local.ALL_INSTANCE_IDS,count.index )
+  port = var.PORT
+}
