6c04b12f2e3783cb7ee78a451799f292be8fccc0
csyed <smachisty92@gmail.com>
2022-03-03 19:34:03 +0530

    terraform

--- alb.tf
+++ alb.tf
@@ -1,5 +1,5 @@
-#resource "aws_lb_target_group" "frontend" {
-#  name = "${var.COMPONENT}-${var.ENV}"
-#  port = 80
-#  protocol = "HTTP"
-#}
+resource "aws_lb_target_group" "frontend" {
+  name = "${var.COMPONENT}-${var.ENV}"
+  port = 80
+  protocol = "HTTP"
+}
