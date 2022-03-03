cdb230ebc1e3d26a9c00768a7a436f5e1fdb5a7e
csyed <smachisty92@gmail.com>
2022-03-03 19:15:34 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "od-instance" {
   count         = var.OD_INSTANCE_COUNT
   instance_type = var.OD_INSTANCE_TYPE
   ami           = data.aws_ami.ami.id
-  subnet_id = element(data.terraform_remote_state.vpc.output.PRIVATE-SUBNETS_ID,count.index)
+  subnet_id = element(data.terraform_remote_state.vpc.outputs.PRIVATE-SUBNETS_ID,count.index)
   vpc_security_group_ids = [aws_security_group.allow.id]
 }
 
@@ -11,7 +11,7 @@ resource "aws_spot_instance_request" "sopt_instance" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment   = true
-  subnet_id  = element(data.terraform_remote_state.vpc.PRIVATE-SUBNETS_ID,count.index)
+  subnet_id  = element(data.terraform_remote_state.vpc.outputs.PRIVATE-SUBNETS_ID,count.index)
   vpc_security_group_ids = [aws_security_group.allow.id]
 #  root_block_device { this is to update the volumn size
 #    volume_size = 10
