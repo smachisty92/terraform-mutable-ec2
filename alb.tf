f9334317806118fa9e9c79f1ee0f56c8a30e32f9
csyed <smachisty92@gmail.com>
2022-03-09 12:44:18 +0530

    terraform

--- alb.tf
+++ alb.tf
@@ -18,7 +18,7 @@ resource "aws_lb_listener" "frontend" {
   port ="80"
   protocol = "HTTP"
   default_action {
-    type = "frontend"
+    type = "forward"
     target_group_arn = aws_lb_target_group.frontend.arn
   }
 }
